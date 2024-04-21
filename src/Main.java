1688d1da65cb1b5f6d08b1f00158958f778305f0
Ella Melnikova <git.user.ellamelnikova04@gmail.com>
2024-04-21 14:19:08 +0700

    Изменение подсчет строк с исключением_

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,52 @@
+import java.io.*;
 import java.util.Scanner;
-import java.io.File;
-
 public class Main
 {
     public static void main(String[] args) {
         int counter = 0;
-        while(true) {
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
+        String path = null;
+        while (true) {
+            String path_temp = new Scanner(System.in).nextLine();
+            File file = new File(path_temp);
             boolean fileexists = file.exists();
             boolean isDirectory = file.isDirectory();
 
-            if ( (!fileexists) || (isDirectory) ) {
-                System.out.println("Файл несуществует или указанный путь - путь к папке.");
+            if ((!fileexists) || (isDirectory)) {
+                System.out.println("Файл несуществует или указанный путь - путь к папкке.");
                 continue;
-            }
-            else  {
+            } else {
                 System.out.println("Путь указан верно");
-                counter = counter + 1;
-                System.out.println("Это файл номер " + counter);
+                path = path_temp;
+                break;
             }
         }
+
+        BufferedReader reader;
+        try {
+            FileReader fileReader = new FileReader(path);
+            reader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String line;
+
+        try {
+            int len_first = reader.readLine().length(), max_length = len_first, min_length = len_first, count = 1;
+            while ((line = reader.readLine()) != null) {
+                if (line.length() >= 1024)
+                    throw new RuntimeException("Line is out of range");
+                count += 1;
+                if (min_length > line.length())
+                    min_length = line.length();
+                if (max_length < line.length())
+                    max_length = line.length();
+            }
+            System.out.print(count + " " + max_length + " " + min_length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
 
