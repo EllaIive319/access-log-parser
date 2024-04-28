f7916e0e2ef18f0e9f77b5171d0bcb38116e5ae2
Ella Melnikova <git.user.ellamelnikova04@gmail.com>
2024-04-28 22:04:26 +0700

    Изменение кода Сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args) {
@@ -29,23 +32,102 @@ public class Main
             throw new RuntimeException(e);
         }
 
+        String[] parseLine = new String[7];
         String line;
-
+        char[] temp;
+        double countLines = 0;
+        double countYandex = 0;
+        double countGoogle = 0;
         try {
-            int len_first = reader.readLine().length(), max_length = len_first, min_length = len_first, count = 1;
             while ((line = reader.readLine()) != null) {
                 if (line.length() >= 1024)
                     throw new RuntimeException("Line is out of range");
-                count += 1;
-                if (min_length > line.length())
-                    min_length = line.length();
-                if (max_length < line.length())
-                    max_length = line.length();
+                for (int i = 0; i<7; i++){
+                    parseLine[i] = "";
+                }
+                temp = line.toCharArray();
+                int i = 0;
+
+                while (temp[i] != ' ') { // IP
+                    parseLine[0] += temp[i];
+                    i++;
+                }
+                while (temp[i] != '[') { // идем до даты-времени
+                    i++;
+                }
+                i++;
+                while (temp[i] != ']') { // Дата-время
+                    parseLine[1] += temp[i];
+                    i++;
+                }
+                i += 3;
+                while (temp[i] != '"') { // Метод Get
+                    parseLine[2] += temp[i];
+                    i++;
+                }
+                i += 2;
+                while (temp[i] != ' ') { // HTTP-ответ
+                    parseLine[3] += temp[i];
+                    i++;
+                }
+                i++;
+                while (temp[i] != ' ') { // Размер в байтах
+                    parseLine[4] += temp[i];
+                    i++;
+                }
+                i += 2;
+                while (temp[i] != '"') { // Путь к странице
+                    parseLine[5] += temp[i];
+                    i++;
+                }
+                i += 3;
+
+                while (temp[i] != '"') { // User-Agent
+                    parseLine[6] += temp[i];
+                    i++;
+                }
+                i = 0;
+                String firstBrackets = "";
+                String User_Agent = parseLine[6];
+                temp = User_Agent.toCharArray();
+                int flag = 0;
+                for (char c : temp) {
+                    if (c == ')')
+                        break;
+                    if (flag == 1)
+                        firstBrackets += c;
+                    if (c == '(')
+                        flag = 1;
+                }
+                String fragment = null;
+                firstBrackets = firstBrackets.replaceAll(" ","");
+                String[] parts = firstBrackets.split(";");
+                if (parts.length >= 2)
+                    fragment = parts[1];
+                else
+                    fragment = "-";
+                String fragment1 = "";
+                for (char c : fragment.toCharArray()){
+                    if (c == '/')
+                        break;
+                    fragment1 += c;
+                }
+                fragment = fragment1;
+                if (fragment.equals("YandexBot"))
+                    countYandex++;
+
+                if (fragment.equals("Googlebot")) {
+                    countGoogle++;
+
+                }
+                countLines++;
+
             }
-            System.out.print(count + " " + max_length + " " + min_length);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        System.out.println("Доля Яндекс ботов" + " " + (countYandex/countLines));
+        System.out.println("Доля Гугл ботов" + " " + (countGoogle/countLines));
     }
 }
