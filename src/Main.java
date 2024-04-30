72ce1acd41b98cbc7bf57541f64a87658a7521be
Ella Melnikova <git.user.ellamelnikova04@gmail.com>
2024-04-30 22:46:53 +0700

    Изменение кода Введение в ООП

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,215 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.abs;
+
+//C:\Course\access.log
+// Введение в ООП
+class LocalDateTime{
+    List<Integer> time = new ArrayList<Integer>();
+    List<String> dateTime = new ArrayList<String>();
+    public LocalDateTime(String date){
+        char[] temp = date.toCharArray();
+        int i = 0;
+        String timeArr="";
+        while (temp[i]!=':') {
+            timeArr += temp[i];
+            i++;
+        }
+        dateTime = List.of(timeArr.split("/"));
+        i++;
+        timeArr="";
+        while (temp[i]!=' ') {
+            timeArr += temp[i];
+            i++;
+        }
+        String[] tempTime = timeArr.split(":");
+        for (i = 0; i<tempTime.length; i++) {
+            this.time.add(parseInt(tempTime[i]));
+        }
+
+    }
+    public String toString(){
+        return this.time.toString() + " " + dateTime;
+    }
+}
+
+class UserAgent {
+    String operationSystem = "";
+    String browser = "";
+
+    public UserAgent(String line){
+        String firstBrackets = "";
+        char[] temp = line.toCharArray();
+        int flag = 0;
+        for (char c : temp) {
+            if (c == ' ')
+                break;
+            this.browser += c;
+        }
+        for (char c : temp) {
+            if (c == ')')
+                break;
+
+            if (flag == 1)
+                firstBrackets += c;
+            if (c == '(')
+                flag = 1;
+        }
+
+        firstBrackets = firstBrackets.replaceAll(" ","");
+        String[] parts = firstBrackets.split(";");
+        if (parts.length >= 1)
+            this.operationSystem = parts[0];
+        else
+            this.operationSystem = "-";
+    }
+    public String toString(){
+        return this.browser+ " " + this.operationSystem;
+    }
+}
+
+enum HttpMethod {
+    GET ("GET"),
+    PUT ("PUT"),
+    POST ("POST");
+    final String method;
+
+    HttpMethod(String method) {
+        this.method = method;
+    }
+}
+
+class LogEntry {
+    final String ipAddr;
+    final LocalDateTime time;
+    final HttpMethod method;
+    final String path;
+    final int responseCode;
+    final int responseSize;
+    final String referer;
+    final UserAgent agent;
+
+    public LogEntry(String line){
+        String[] parseLine = new String[8];
+
+        for (int i = 0; i<8; i++){
+            parseLine[i] = "";
+        }
+        char[] temp = line.toCharArray();
+        int i = 0;
+
+        while (temp[i] != ' ') { // IP
+            parseLine[0] += temp[i];
+            i++;
+        }
+        while (temp[i] != '[') { // идем до даты-времени
+            i++;
+        }
+        i++;
+        while (temp[i] != ']') { // Дата-время
+            parseLine[1] += temp[i];
+            i++;
+        }
+        i += 3;
+        while (temp[i] != ' ') { // Метод Get
+            parseLine[2] += temp[i];
+            i++;
+        }
+        i++;
+        while (temp[i] != ' ') { // Метод Get
+            parseLine[3] += temp[i];
+            i++;
+        }
+        while (temp[i] != '"') { // Метод Get
+            i++;
+        }
+        i += 2;
+        while (temp[i] != ' ') { // HTTP-ответ
+            parseLine[4] += temp[i];
+            i++;
+        }
+        i++;
+        while (temp[i] != ' ') { // Размер в байтах
+            parseLine[5] += temp[i];
+            i++;
+        }
+        i += 2;
+        while (temp[i] != '"') { // Путь к странице
+            parseLine[6] += temp[i];
+            i++;
+        }
+        i += 3;
+
+        while (temp[i] != '"') { // User-Agent
+            parseLine[7] += temp[i];
+            i++;
+        }
+        this.ipAddr = parseLine[0];
+        this.time = new LocalDateTime(parseLine[1]);
+        this.method = HttpMethod.valueOf(parseLine[2]);
+        this.referer = parseLine[3];
+        this.responseCode = parseInt(parseLine[4]);
+        this.responseSize = parseInt(parseLine[5]);
+        this.path = parseLine[6];
+        this.agent = new UserAgent(parseLine[7]);
+    }
+    public String getIpAddr() {
+        return this.ipAddr;
+    }
+    public LocalDateTime getTime() {
+        return this.time;
+    }
+    public HttpMethod getMethod() {
+        return this.method;
+    }
+    public String getReferer() {
+        return this.referer;
+    }
+    public int getResponseCode() {
+        return this.responseCode;
+    }
+    public int getResponseSize() {
+        return this.responseSize;
+    }
+    public UserAgent getAgent() {
+        return this.agent;
+    }
+    public String getPath() {
+        return this.path;
+    }
+
+
+    public String toString(){
+        return this.ipAddr+" "+this.time+" "+this.method+" "+this.referer+" "+this.responseCode+" "+this.responseSize+" "+this.path+" "+this.agent;
+    }
+}
+
+class Statistics {
+    long totalTraffic;
+    LocalDateTime minTime;
+    LocalDateTime maxTime;
+
+    public Statistics(){
+        this.totalTraffic = 0;
+        this.maxTime = new LocalDateTime("25/Sep/2022:06:25:04 +0300");
+        this.minTime = new LocalDateTime("25/Sep/2022:06:25:04 +0300");
+    }
+
+    public void addEntry(LogEntry LE){
+        this.totalTraffic += LE.getResponseSize();
+        if ((this.minTime.time.get(0) > LE.getTime().time.get(0)) && (parseInt(this.minTime.dateTime.get(0)) >= parseInt(LE.getTime().dateTime.get(0))))
+            this.minTime = LE.getTime();
+        if ((this.maxTime.time.get(0) < LE.getTime().time.get(0)) && (parseInt(this.maxTime.dateTime.get(0)) <= parseInt(LE.getTime().dateTime.get(0))))
+            this.maxTime = LE.getTime();
+    }
+
+    public double getTrafficRate() {
+        double hours = abs(24*(parseInt(this.maxTime.dateTime.get(0)) - parseInt(this.minTime.dateTime.get(0))) - (this.maxTime.time.get(0) - this.minTime.time.get(0)));
+        return hours/this.totalTraffic;
+    }
+}
 public class Main
 {
     public static void main(String[] args) {
@@ -32,102 +239,27 @@ public class Main
             throw new RuntimeException(e);
         }
 
-        String[] parseLine = new String[7];
+
         String line;
-        char[] temp;
-        double countLines = 0;
-        double countYandex = 0;
-        double countGoogle = 0;
+        List<LogEntry> LE = new ArrayList<LogEntry>();
+        Statistics stat = new Statistics();
+        int i = 0;
         try {
             while ((line = reader.readLine()) != null) {
                 if (line.length() >= 1024)
                     throw new RuntimeException("Line is out of range");
-                for (int i = 0; i<7; i++){
-                    parseLine[i] = "";
-                }
-                temp = line.toCharArray();
-                int i = 0;
-
-                while (temp[i] != ' ') { // IP
-                    parseLine[0] += temp[i];
-                    i++;
-                }
-                while (temp[i] != '[') { // идем до даты-времени
-                    i++;
-                }
+                LogEntry LElement = new LogEntry(line);
+                LE.add(i, LElement);
+                stat.addEntry(LElement);
+                //System.out.println(LElement.getAgent());
                 i++;
-                while (temp[i] != ']') { // Дата-время
-                    parseLine[1] += temp[i];
-                    i++;
-                }
-                i += 3;
-                while (temp[i] != '"') { // Метод Get
-                    parseLine[2] += temp[i];
-                    i++;
-                }
-                i += 2;
-                while (temp[i] != ' ') { // HTTP-ответ
-                    parseLine[3] += temp[i];
-                    i++;
-                }
-                i++;
-                while (temp[i] != ' ') { // Размер в байтах
-                    parseLine[4] += temp[i];
-                    i++;
-                }
-                i += 2;
-                while (temp[i] != '"') { // Путь к странице
-                    parseLine[5] += temp[i];
-                    i++;
-                }
-                i += 3;
-
-                while (temp[i] != '"') { // User-Agent
-                    parseLine[6] += temp[i];
-                    i++;
-                }
-                i = 0;
-                String firstBrackets = "";
-                String User_Agent = parseLine[6];
-                temp = User_Agent.toCharArray();
-                int flag = 0;
-                for (char c : temp) {
-                    if (c == ')')
-                        break;
-                    if (flag == 1)
-                        firstBrackets += c;
-                    if (c == '(')
-                        flag = 1;
-                }
-                String fragment = null;
-                firstBrackets = firstBrackets.replaceAll(" ","");
-                String[] parts = firstBrackets.split(";");
-                if (parts.length >= 2)
-                    fragment = parts[1];
-                else
-                    fragment = "-";
-                String fragment1 = "";
-                for (char c : fragment.toCharArray()){
-                    if (c == '/')
-                        break;
-                    fragment1 += c;
-                }
-                fragment = fragment1;
-                if (fragment.equals("YandexBot"))
-                    countYandex++;
-
-                if (fragment.equals("Googlebot")) {
-                    countGoogle++;
-
-                }
-                countLines++;
-
+                //break;
             }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Доля Яндекс ботов" + " " + (countYandex/countLines));
-        System.out.println("Доля Гугл ботов" + " " + (countGoogle/countLines));
+        System.out.println(stat.getTrafficRate());
+
     }
 }
